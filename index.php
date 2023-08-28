b48af9669b2ee3af5d1f17a0514faa16b8792307
nahid-dev <nahidv232@gmail.com>
2023-08-28 14:33:32 +0600

    basic variable

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    $age = 10;
+    $_age = 10;
+    $Age = 10;
+    $AGE = 10;
+    $age1 = 10;
+    $age = 10;
+
+    echo $age;
+
+    ?>
+</body>
+
+</html>
